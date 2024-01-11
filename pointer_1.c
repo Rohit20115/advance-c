b3ac29bb2667ef3110b4277fd1c67cfcf8556ce3
Rohit sahu <143478312+Rohit20115@users.noreply.github.com>
2024-01-11 09:53:58 +0530

    Add files via upload

---
+++ pointer_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    int *b;
+    b=&a;
+
+    printf("value of a = %d\n",a);
+    printf("value of a = %d\n",(&a));
+    printf("value of a = %d\n",*b);
+    printf("address of a = %d\n",&a);
+    printf("address of a = %d\n",b);
+    printf("address of b = %d\n",&b);
+    printf("address of b = address of a = %u",b);
+    return 0 ;
+
+}
+

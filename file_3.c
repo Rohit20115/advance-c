7b77df7e39e86219c993321b16f7eba10b0aa557
Rohit sahu <143478312+Rohit20115@users.noreply.github.com>
2024-01-12 20:32:06 +0530

    Add files via upload

---
+++ file_3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+
+        struct node *new;
+        new = (struct node*)malloc(size of(struct node));
+        new -> data = 10;
+        new ->next = null;
+         
+}

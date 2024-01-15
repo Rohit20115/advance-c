232997683a0bf6556b18a7ac5b74f27d3bed729e
Rohit sahu <143478312+Rohit20115@users.noreply.github.com>
2024-01-15 16:28:27 +0530

    Add files via upload

---
+++ pointeraray.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+
+        char *city[3]={"Rewa","satna","goa"};
+        int i;
+
+        for(i=0;i<3;i++)
+        {
+            printf("\n%s",&city[i]);
+
+        }
+}
